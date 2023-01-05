781a59503f3e4e88178048a04afe1fc58a8cfc3c
Wildan M <willnode@wellosoft.net>
2023-01-05 23:43:04 +0700

    Add france to map

--- src/components/home/Home6.tsx
+++ src/components/home/Home6.tsx
@@ -27,6 +27,15 @@ export default function Home6() {
                     }} >
                         <FaMapMarker />
                     </a>
+                    <a href="#fr1" style={{
+                        position: 'absolute',
+                        top: '28.4%',
+                        left: '47.4%',
+                        width: '25px',
+                        transform: 'translate(-11px, -45px)',
+                    }} >
+                        <FaMapMarker />
+                    </a>
                 </div>
                 <p className="text--center"><a href="https://github.com/domcloud/domcloud/issues/22" target="_blank" rel="noopener noreferrer">Psst... Want more location?</a></p>
             </div>
